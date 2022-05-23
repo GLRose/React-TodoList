50679c885aacf0836e5e1ca78ca8a7b9e16761d2
Garrett Rose <roseg3@winthrop.edu>
2022-05-23 10:45:23 -0400

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./style.css"
+const Header = () => {
+  return (
+    <div>
+      <h1 className='header'>User Diets</h1>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/TestObjects.js
@@ -0,0 +1,48 @@
+import { useState, useRef, useEffect } from "react";
+import "./style.css"
+
+const TestObjects = () => {
+  //State to delete the current person added to the list
+  const [deleted, deletePerson] = useState(null);
+  const[newPerson, setNewPerson] = useState('');
+  //Declare our ref, in this case the user input will be our ref
+  const textInput = useRef();
+  //When the button is clicked we will use state to set our input
+  //you must use .current to access ref values... We can then access our ref through "textInput"
+  const handleClick = () => {
+    setNewPerson(textInput.current.value);
+  }
+  
+  const remove = (newPerson) => {
+    newPerson = setNewPerson(null);
+  }
+
+  useEffect(() => {
+    localStorage.setItem(newPerson, String(newPerson))
+    console.log(newPerson)
+  }, [newPerson])
+  const users = [
+    { id: 1, name: "Garrett", Diet: "vegetarian" },
+    { id: 2, name: "Stella", Diet: "vegan" },
+    { id: 3, name: "Lori", Diet: "Paleo" },
+    { id: 4, name: "Dexter", Diet: "Hills Science Diet"}
+  ];
+
+  return (
+    <div className="user">
+      {users.map((user) => (
+        <p key={user.id}>
+          {user.name}
+          {":"} {user.Diet} <input type='checkbox'/>
+        </p>
+      ))}
+
+      <input className="userInput" type="text" ref={textInput}></input>
+      <button className="myButtons" onClick={handleClick}>Create New Entry</button>
+      <button className="myButtons" onClick={remove}>Remove Person</button>
+      <p className="userText" style={{'textDecoration':'line-through'}}>{newPerson}</p>
+    </div>
+  );
+};
+
+export default TestObjects;
